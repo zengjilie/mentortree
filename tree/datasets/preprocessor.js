14b2b7bd47c5b2e5aa9e41328d2197778473bdb6
zengjilie <zengjilie@gmail.com>
2022-04-05 11:31:05 -0500

    finished preprocessor

--- tree/datasets/preprocessor.js
+++ tree/datasets/preprocessor.js
@@ -1,6 +1,40 @@
 const data = require("./psych_id783121.json");
-const root = data;
+// const data = {
+//     name: 'Gege',
+//     gender: 'man',
+//     gender_color: 'blue',
+//     children: [
+//         {
+//             name: 'Jiejie',
+//             gender: 'man',
+//             gender_color: 'blue',
+//             children: [
+//                 {
+//                     name: 'Meimei',
+//                     gender: 'man',
+//                     gender_color: 'blue',
+//                 },
+//                 {
+//                     name: 'Didi',
+//                     gender: 'woman',
+//                     gender_color: 'red',
+//                 },
+//             ]
+//         },
+//         {
+//             name: 'Dama',
+//             gender: 'woman',
+//             gender_color: 'red',
+//         },
+//         {
+//             name: 'Dashu',
+//             gender: 'woman',
+//             gender_color: 'red',
+//         },
+//     ]
+// }
 
+const root = data;
 /**
  * 1. store all the researcher's name and their children number inside map
  * 2. map children number to precentage
@@ -13,10 +47,62 @@ let map = new Map();
 
 // the recurse function will find the reseracher's name and their chidrenNum;
 function recurse(root) {
+    //base case
+    if (root?.children == null) {
+        map.set(root.name, 1);
+        return 1;
+    } else {
+        let num = 0;
+        for (let i = 0; i < root.children.length; i++) {
+            const childNum = recurse(root?.children[i]);
+            num += childNum;
+        }
+        map.set(root.name, num);
+        return num + 1;
+
+    }
 
 }
 
-recurse();
+recurse(root);
+// console.log(map);
 
-//
+// === get precentage ===
+
+// calculate sum
+let sum = 0;
+for (let [key, value] of map) {
+    sum += value;
+}
+
+for (let [key, value] of map) {
+    const prec = value / sum;
+    map.set(key, prec);
+}
+
+//=== add weight to data ===
+
+function traverse(root) {
+    if (root?.children == null) {
+        root.weight = map.get(root.name);
+        return;
+    } else {
+        root.weight = map.get(root.name);
+        for (let i = 0; i < root.children.length; i++) {
+            console.log(root.name);
+            traverse(root?.children[i]);
+        }
+        return;
+    }
+}
+
+traverse(root);
+
+// === export new data ===
+
+let weightedData = JSON.stringify(root);
+var fs = require('fs');
+fs.writeFile("psych_id783121_new.json", weightedData, function (err) {
+    if (err) console.log('error', err);
+});
 
