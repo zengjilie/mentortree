4fe33c2471aa10996a80a328591ceeb40ca870fb
zengjilie <zengjilie@gmail.com>
2022-05-18 02:55:28 -0500

    updated new algorithm

--- tree/preprocessor.js
+++ tree/preprocessor.js
@@ -56,9 +56,9 @@
 //Special
 // const data = require("./special/curly-tree.json");
 // const data = require("./special/female-titled-tree.json");
-// const data = require("./special/male-tilted-tree.json");
+const data = require("./special/male-tilted-tree.json");
 // const data = require("./special/tallest-tree.json");
-const data = require("./special/widest-tree.json");
+// const data = require("./special/widest-tree.json");
 const root = data;
 
 //< Procedure >
@@ -83,7 +83,7 @@ let areaSet = new Set();
  * children:researcher[] 
  *  })
  * recurse function will find the reseracher's name and their chidrenNum
- * */ 
+ * */
 
 function recurse(root) {
     //< Warning: the "researcharea" property may not a string array, but a string >
@@ -114,7 +114,7 @@ function recurse(root) {
 recurse(root);
 // console.log(areaSet);
 
-// < Add all researchareas to root node >
+// < Add a new property "allResearchAreas" to >
 root.allResearchAreas = [...areaSet];
 
 // < Calculate the "precentage"
@@ -128,7 +128,7 @@ root.allResearchAreas = [...areaSet];
 
 let sum = 0;
 let max = 0;
-let min = 0;
+let min = 1;
 let mean = 0;
 for (let [key, value] of map) {
     sum += value;
@@ -141,39 +141,35 @@ mean = sum / map.size;
 console.log('sum: ' + sum);
 console.log('map size:' + map.size);
 console.log('mean:' + mean);
+console.log('max:' + max);
+console.log('min:' + min);
 console.log(map);
 
-//1. divided by sum
-// for (let [key, value] of map) {
-//     const prec = value / sum; //     map.set(key, prec);
-// }
-
-//2. divided by max
-// for (let [key, value] of map) {
-//     const prec = value / max ;
-//     map.set(key, prec);
-// }
-
-// 3. |mean - x|/|x_max - x_min| ( x is childNum)
+//  |mean - x|/|x_max - x_min| ( x is childNum)
 for (let [key, value] of map) {
+
     /**
-     * 1. threshold tunning
-     * 2. mean + {x} - value, x increase -> branch thicker
+     * 1. oversampling/undersampling
+     * 2. mean + {sigma} - value, x increase -> branch thicker
      */
-    // let distFromMean = Math.abs(mean + 10 - value);//Francis/hery
-    // let distFromMean = Math.abs(mean + 100 - value);//william;
-    let distFromMean = Math.abs(mean - value);//geroge church/donna/jennifer
 
-    //normalize root 
-    distFromMean = distFromMean > 1000 ? distFromMean / 4 : distFromMean;// Francis / william / tall
-    // distFromMean = distFromMean > 50 ? distFromMean / 4 : distFromMean; // geroge church/hery
-    // distFromMean = distFromMean > 15 ? distFromMean / 4 : distFromMean; // donnaHarway/jennifer
-    // distFromMean = distFromMean > 10 ? distFromMean / 4 : distFromMean; // female tilted tree
-    // distFromMean = distFromMean > 4 ? distFromMean / 4 : distFromMean; // male tilted tree
+    let newValue;
+    //If value bigger then the mean * {x} --> undersampling
+    if (value > mean * 0.8) {
+        const distFromMean = value - mean * 0.8;
+        newValue = value - distFromMean * 4 / 5;
 
+        //If value less then the mean * {y} --> oversampling
+    } else if (value < mean * 0.5) {
+        const distFromMean = value - mean * 0.5;
+        newValue = value + distFromMean / 2;
+    } else {
+        newValue = value;
+    }
 
     let max_min = Math.abs(max - min);
-    const prec = distFromMean / max_min;
+
+    const prec = newValue / max_min;
     map.set(key, prec);
 }
 
@@ -237,9 +233,9 @@ fs = require('fs');
 //Special path
 // const path = "./special-new/curly-tree.json";
 // const path = "./special-new/female-titled-tree.json";
-// const path = "./special-new/male-titled-tree.json";
+const path = "./special-new/male-titled-tree.json";
 // const path = "./special-new/tallest-tree.json";
-const path = "./special-new/widest-tree.json";
+// const path = "./special-new/widest-tree.json";
 
 // Write File
 fs.writeFile(path, weightedData, function (err) {
